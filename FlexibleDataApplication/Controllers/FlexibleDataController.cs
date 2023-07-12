0099c67fd9a9dce302a39a31bb48fc326bbf3c0a
Chandimal Adikari <amchandimal@gmail.com>
2023-07-12 23:29:03 +0530

    Initial Commit

---
+++ FlexibleDataApplication/Controllers/FlexibleDataController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace FlexibleDataApplication.Controllers
+{
+    [ApiController]
+    [Route("flexibledata")]
+    public class FlexibleDataController : ControllerBase
+    {
+
+        [HttpPost("create")]
+        public async Task<IActionResult> createFlexibleData(string jsonString)
+        {
+            var dict = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+            foreach (var kv in dict)
+            {
+                Console.WriteLine(kv.Key + "-" + kv.Value);
+            }
+
+            return Ok();
+        }
+
+    }
+}
---
+++ FlexibleDataApplication/Entities/FlexibleData.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FlexibleDataApplication.Entities
+{
+    public class FlexibleData
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Data { get; set; }
+    }
+}
